68f6d023c280865a1c24e94eaee15c9826ce2248
JoelHer <JoelHerbst07@gmail.com>
2023-01-10 09:09:20 +0100

    Changed main.js to expose all content in /www/

--- main.js
+++ main.js
@@ -2,49 +2,13 @@ const express = require('express')
 const fs = require('fs')
 const https = require('https')
 const app = express()
+const path = require('path')
 const securePort = 443
 const port = 80
 
 const RUNMODE = "DEV" //DEV or PRODUCTION
 
-app.get('/www/favicon.jpg', (req, res) => {
-  res.sendFile('/root/webserver/www/favicon.jpg');
-})
-
-app.get('/src/navbar.png', (req, res) => {
-  res.sendFile('/root/webserver/www/src/navbar.png');
-})
-
-app.get('/fonts/Minecraft-Regular.ttf.eot', (req, res) => {
-  res.sendFile('/root/webserver/www/fonts/Minecraft-Regular.ttf.eot');
-})
-app.get('/fonts/Minecraft-Regular.ttf.svg', (req, res) => {
-  res.sendFile('/root/webserver/www/fonts/Minecraft-Regular.ttf.svg');
-})
-app.get('/fonts/Minecraft-Regular.ttf.woff', (req, res) => {
-  res.sendFile('/root/webserver/www/fonts/Minecraft-Regular.ttf.woff');
-})
-
-app.get('/images/main.png', (req, res) => {
-  res.sendFile('/root/webserver/www/images/main.png');
-})
-
-app.get('/sitemap.txt', (req, res) => {
-  res.sendFile('/root/webserver/sitemap.txt');
-})
-
-app.get('/css/index.css', (req, res) => {
-  res.sendFile('/root/webserver/www/css/index.css');
-})
-
-app.get('/www/js/index.jsx', (req, res) => {
-  res.sendFile('/root/webserver/www/js/index.jsx');
-})
-
-app.get('/googleecd4c64bd68c095c.html', (req, res) => {
-  res.sendFile('/root/webserver/googleecd4c64bd68c095c.html');
-})
-
+app.use(express.static(path.join(__dirname, 'www')))
 
 app.get('/server', (req, res) => {
   res.send('Server Page')
@@ -62,11 +26,8 @@ app.get('/community', (req, res) => {
   res.send('Community Page')
 })
 
-
-
-
 app.get('/', (req, res) => {
-  res.sendFile('/root/webserver/www/index.html');
+  res.sendFile(path.join(__dirname, '/www/index.html'));
 })
 
 if (RUNMODE.toLowerCase() == "production") {
